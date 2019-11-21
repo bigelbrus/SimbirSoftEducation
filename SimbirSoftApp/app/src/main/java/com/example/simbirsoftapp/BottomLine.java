10d237f835763e5cbeeb39f831f08fe02bfff06f
Alex Levchenko <bigelbrus@gmail.com>
2019-11-21 13:54:47 +0500

    Fourth block. Layouts. Corrections #2
    Aded dimens. Changed names of dimens. Removed access to Resources from the onDraw method to the class constructor. Added all arguments values as constants.

--- SimbirSoftApp/app/src/main/java/com/example/simbirsoftapp/BottomLine.java
+++ SimbirSoftApp/app/src/main/java/com/example/simbirsoftapp/BottomLine.java
@@ -9,29 +9,61 @@ import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.toRadians;
+
+
+
 public class BottomLine extends View {
 
     private final Paint paint = new Paint();
     private final RectF oval = new RectF();
-    private Resources res;
+    private float circleDiam;
+    private float buttonPadding;
+    private float width;
+    private int whiteColor;
+    private int greyColor;
+
+    private final float START_ANGLE_MIDDLE_ARC = 200f;      //must be between 180 and 270
+    private final float SWEEP_ANGLE_EDGE_ARC = 20f;
+    private final float SWEEP_ANGLE_MIDDLE_ARC = 2 * (270 - START_ANGLE_MIDDLE_ARC);
+    private final float START_ANGLE_START_ARC = START_ANGLE_MIDDLE_ARC - SWEEP_ANGLE_EDGE_ARC;
+    private final float START_ANGLE_END_ARC = START_ANGLE_MIDDLE_ARC + SWEEP_ANGLE_MIDDLE_ARC;
+    private final float START_ANGLE_STROKE_BUTTON_PADDING = 190f;
+    private final float SWEEP_ANGLE_STROKE_BUTTON_PADDING = 160f;
+
+    private final double ANGLE_HELPER = toRadians(START_ANGLE_MIDDLE_ARC - 180f);
+
+    private final int ALPHA_LIGHT = 60;
+    private final int ALPHA_MEDIUM = 140;
+
+    private final int STROKE_THIN = 3;
+
 
     public BottomLine(Context context, AttributeSet set) {
         super(context, set);
-        init(context);
+        init();
     }
 
-    private void init(Context context){
+    private void init(){
         this.setBackgroundColor(Color.TRANSPARENT);
-        res = context.getResources();
+        Resources res = getResources();
+
+        width = res.getDisplayMetrics().widthPixels;
+        circleDiam = res.getDimensionPixelSize(R.dimen.circle_diam);
+        buttonPadding = res.getDimensionPixelSize(R.dimen.button_padding);
+
+        whiteColor = res.getColor(R.color.white);
+        greyColor = res.getColor(R.color.grey);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        float width = res.getDisplayMetrics().widthPixels;
+
         float center_x = width / 2;
-        float circleDiam = getResources().getDimensionPixelSize(R.dimen.circle_diam);
-        float buttonPadding = getResources().getDimensionPixelSize(R.dimen.button_padding);
         float center_y = (circleDiam / 2) + (2 * buttonPadding);
         float radius = (circleDiam / 2) + (2 * buttonPadding);
         float left = center_x - radius;
@@ -43,27 +75,31 @@ public class BottomLine extends View {
 
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(buttonPadding);
-        paint.setColor(getResources().getColor(R.color.white));
-        canvas.drawArc(oval, 190, 160, false, paint);
+        paint.setColor(whiteColor);
+        canvas.drawArc(oval,
+                START_ANGLE_STROKE_BUTTON_PADDING,
+                SWEEP_ANGLE_STROKE_BUTTON_PADDING,
+                false,
+                paint);
 
-        paint.setColor(getResources().getColor(R.color.grey));
-        paint.setStrokeWidth(3);
+        paint.setColor(greyColor);
+        paint.setStrokeWidth(STROKE_THIN);
         paint.setAntiAlias(true);
-        paint.setAlpha(60);
-        canvas.drawArc(oval, 180, 20, false, paint);
-        canvas.drawArc(oval, 340, 20, false, paint);
+        paint.setAlpha(ALPHA_LIGHT);
+        canvas.drawArc(oval, START_ANGLE_START_ARC, SWEEP_ANGLE_EDGE_ARC, false, paint);
+        canvas.drawArc(oval, START_ANGLE_END_ARC, SWEEP_ANGLE_EDGE_ARC, false, paint);
         canvas.drawLine(0,
-                center_y - (float) Math.sin(Math.toRadians(20)) * radius,
-                center_x - (float) Math.cos(Math.toRadians(20)) * radius,
-                center_y - (float) Math.sin(Math.toRadians(20)) * radius,
+                center_y - (float) sin(ANGLE_HELPER) * radius,
+                center_x - (float) cos(ANGLE_HELPER) * radius,
+                center_y - (float) sin(ANGLE_HELPER) * radius,
                 paint);
-        canvas.drawLine(center_x + (float) Math.cos(Math.toRadians(20)) * radius,
-                center_y - (float) Math.sin(Math.toRadians(20)) * radius,
+        canvas.drawLine(center_x + (float) cos(ANGLE_HELPER) * radius,
+                center_y - (float) sin(ANGLE_HELPER) * radius,
                 width,
-                center_y - (float) Math.sin(Math.toRadians(20)) * radius,
+                center_y - (float) sin(ANGLE_HELPER) * radius,
                 paint);
 
-        paint.setAlpha(140);
-        canvas.drawArc(oval, 200, 140, false, paint);
+        paint.setAlpha(ALPHA_MEDIUM);
+        canvas.drawArc(oval, START_ANGLE_MIDDLE_ARC, SWEEP_ANGLE_MIDDLE_ARC, false, paint);
     }
 }
